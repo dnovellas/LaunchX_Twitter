a3cf7bc169a5ec859d7e13c6b37e6c8386ffac26
David Novella Saldana <dnovella@Novella-Inspiron.localdomain>
2022-05-02 14:07:16 -0500

    Se agrega requerimiento 3

--- Twitter/test/models/User.test.js
+++ Twitter/test/models/User.test.js
@@ -1,16 +1,12 @@
 const User = require('./../../app/models/User')
 describe("Unit Test for User class", ()=>{
-  test('Create an User object', ()=>{
+  test('Add getters', ()=>{
     //Aquí invocas el código que vas a usar en tu app
-    const user = new User(1, "carlogilmar", "Carlo", "Bio");
-
-    //Aquí validas los resultados de ese código
-    // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
-    expect(user.id).toBe(1);
-    expect(user.username).toBe("carlogilmar");
-    expect(user.name).toBe("Carlo");
-    expect(user.bio).toBe("Bio");
-    expect(user.dateCreated).not.toBeUndefined(); // Verifica que el valor no sea undefined;
-    expect(user.lastUpdated).not.toBeUndefined();
+    const user = new User(1, "carlogilmar", "Carlo", "Bio");    
+    
+    expect(user.getUsername).toBe("carlogilmar");    
+    expect(user.getBio).toBe("Bio");
+    expect(user.getDateCreated).not.toBeUndefined(); // Verifica que el valor no sea undefined;
+    expect(user.getLastUpdated).not.toBeUndefined();
   })
 })
