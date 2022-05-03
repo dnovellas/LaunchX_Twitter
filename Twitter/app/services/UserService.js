bbc054d877325b30651bcee152ff7b2f7915c50b
David Novella Saldana <dnovella@Novella-Inspiron.localdomain>
2022-05-02 19:25:14 -0500

    Se agrega ejercicio #4, Sprint #2 Requerimiento #2

--- Twitter/app/services/UserService.js
+++ Twitter/app/services/UserService.js
@@ -4,6 +4,15 @@ class UserService{
   static create(id, username, name){
     return new User(id, username, name, "Sin bio")
   }
+
+  static getInfo(user){
+    let userInfoInList = [];
+    userInfoInList[0] = user.id;
+    userInfoInList[1] = user.getUsername;;
+    userInfoInList[2] = user.name;
+    userInfoInList[3] = user.getBio;
+    return userInfoInList;
+  }
 }
 
 module.exports = UserService
--- Twitter/test/services/UserService.test.js
+++ Twitter/test/services/UserService.test.js
@@ -1,11 +1,23 @@
+const User = require('../../app/models/User')
 const UserService = require('./../../app/services/UserService')
 
+// describe("Test for UserService", ()=>{
+//   test("1. Create a new user using UserService", ()=>{
+//     const user = UserService.create(1, "carlogilmar", "Carlo")
+//     expect(user.username).toBe("carlogilmar")
+//     expect(user.name).toBe("Carlo")
+//     expect(user.id).toBe(1)
+//     expect(user.bio).not.toBeUndefined()
+//   })
+// })
+
 describe("Test for UserService", ()=>{
-  test("1. Create a new user using UserService", ()=>{
+  test("2. Get all user data in a list", ()=>{
     const user = UserService.create(1, "carlogilmar", "Carlo")
-    expect(user.username).toBe("carlogilmar")
-    expect(user.name).toBe("Carlo")
-    expect(user.id).toBe(1)
-    expect(user.bio).not.toBeUndefined()
+    const userInfoInList = UserService.getInfo(user)
+    expect(userInfoInList[0]).toBe(1)
+    expect(userInfoInList[1]).toBe("carlogilmar")
+    expect(userInfoInList[2]).toBe("Carlo")    
+    expect(userInfoInList[3]).not.toBeUndefined()
   })
 })
