3c921c55f82311f01f35b815ec5eaefac680db32
David Novella Saldana <dnovella@Novella-Inspiron.localdomain>
2022-05-03 23:58:58 -0500

    Se agrega el requerimiento #2

--- Twitter/app/views/UserView.js
+++ Twitter/app/views/UserView.js
@@ -3,12 +3,43 @@ const UserService = require('./../../app/services/UserService')
 class UserView{
   static createUser(payload){
     const result = {
-      error : ""
+      error : "",
+      user: null
     };
 
-    if(payload === null)
-      result.error = "payload no existe";
-      return result;
+    if(payload === null){
+        result.error = "payload no existe";      
+    }else{
+      // Validate payload
+      if(this.validatePayload(payload)){
+        result.user = UserService.create(payload.id, payload.username, payload.name);
+      }else
+      {
+        result.error = "necesitan tener un valor válido";
+      }
+    }
+    return result;
+  }
+
+  static validatePayload(payload){
+    let isPayloadValid = false;
+    
+    // validate payload.id
+    if (payload.id != null && payload.id != "" && !isNaN(payload.id)) {
+      isPayloadValid = true;
+    }
+
+    // validate payload.name
+    if (payload.name != null && payload.name != "" && isNaN(payload.name)) {
+      isPayloadValid = true;
+    }
+
+    //validate payload.username
+    if (payload.username != null && payload.username != "" && isNaN(payload.username)) {
+      isPayloadValid = true;
+    }
+
+    return isPayloadValid;
   }
 }
 
--- Twitter/test/views/UserView.test.js
+++ Twitter/test/views/UserView.test.js
@@ -1,10 +1,19 @@
 const UserView = require('./../../app/views/UserView')
 
+// describe("Test for UserView", ()=>{
+//   test("Return an error object when try to create a new user with an null payload", ()=>{
+//     const payload = null;
+//     const result = UserView.createUser(payload)   
+//     //httops://jestjs.io/docs/using-matchers#strings
+//     expect(result.error).toMatch(/payload no existe/)    
+//   })
+// })
+
 describe("Test for UserView", ()=>{
-  test("Return an error object when try to create a new user with an null payload", ()=>{
-    const payload = null;
+  test("Return an error object when try to create a new user with payload with invalid properties", ()=>{
+    const payload = {username:null, name:12, id:"id"};
     const result = UserView.createUser(payload)   
     //httops://jestjs.io/docs/using-matchers#strings
-    expect(result.error).toMatch(/payload no existe/)    
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
   })
 })
